050d8ac383621fd28bb7956fb3c4b89568690c6d
Kawan <43068452+Kawan@users.noreply.github.com>
2021-09-09 22:18:09 +0200

    Update main.lua
    This will stop minimap flashing in x seconds

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,5 @@
 Citizen.CreateThread(function()
-	Citizen.Wait(100)
-
 	while true do
-		local sleepThread = 500
-
 		local radarEnabled = IsRadarEnabled()
 
 		if not IsPedInAnyVehicle(PlayerPedId()) and radarEnabled then
@@ -11,7 +7,6 @@ Citizen.CreateThread(function()
 		elseif IsPedInAnyVehicle(PlayerPedId()) and not radarEnabled then
 			DisplayRadar(true)
 		end
-
-		Citizen.Wait(sleepThread)
+		Citizen.Wait(1)
 	end
 end)
